6be011e02cc526c35956b97f7c4f501b7fdb5cff
mmmarzouki <31728512+mmmarzouki@users.noreply.github.com>
2017-11-25 22:13:08 -0800

    PeerController

---
+++ app/Http/Controllers/PeerController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Peer;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class PeerController extends Controller
+{
+    public function create(Request $request) {
+
+        $validator=\Validator::make($request,[
+            'name'=> 'required|alpha',
+            'goal'=>'required|alpha_num',
+            'deadline'=>'required|alpha_num',
+            'level'=>'required|alpha',
+            'type'=>'required|alpha',
+            'icon'=>'required'
+        ]);
+        if($validator->fails()){
+            return response()->bad_request_exception();
+        }
+        $peer = new Peer();
+
+        foreach ($request->all() as $key => $value){
+            if(inArray($key,$peer->getColumns())&& !is_null($value)){
+                $peer->$key=$value;
+            }
+        }
+        $peer->saveOrFail();
+
+        return response()->created();
+    }
+
+    public function delete(Request $request){
+
+
+    }
+    public function read(Request $request){
+        $id=$request->get('id');
+        if(is_null($id)){
+            return response()->bad_request_exception();
+        }
+        $peer= Peer::find($id);
+
+        return $peer;
+    }
+    public function readAll(Request $request){
+        return(Peer::all());
+    }
+    public function update(Request $request){
+        $validator=\Validator::make($request,[
+            'name'=> 'required|alpha',
+            'goal'=>'required|alpha_num',
+            'deadline'=>'required|alpha_num',
+            'level'=>'required|alpha',
+            'type'=>'required|alpha',
+            'icon'=>'required'
+        ]);
+        if($validator->fails()){
+            return response()->bad_request_exception();
+        }
+        $id=$request->get('id');
+        if(is_null($id)){
+            return response()->bad_request_exception();
+        }
+        $peer= Peer::find($id);
+
+        foreach ($request->all() as $key => $value){
+            if(inArray($key,$peer->getColumns())&& !is_null($value)){
+                $peer->$key=$value;
+            }
+        }
+        $peer->saveOrFail();
+
+        return response()->created();
+    }
+}
