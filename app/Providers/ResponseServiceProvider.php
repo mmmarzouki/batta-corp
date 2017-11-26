dceab66c4fa4b42c96bdbec3cf82320ddb3d28a3
ghassen <manai.ghassen02@gmail.com>
2017-11-26 03:31:14 +0100

    121

--- app/Providers/ResponseServiceProvider.php
+++ app/Providers/ResponseServiceProvider.php
@@ -13,40 +13,68 @@ class ResponseServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        \Response::macro('api',function($code = 200, $message = '', $data = null) {
+        \Response::macro('api',function($message = 'Status Ok', $data = null) {
             $resp = \Response::make();
             $content = new JsonResponseContent($_code = $code, $_message = $message, $_data = $data );
             $resp->setContent($content);
-            $resp->setStatusCode($code,$message);
+            $resp->setStatusCode(200,$message);
             return $resp;            
         });
         
         \Response::macro('created',function($message = 'Ressource Created', $data = null) {
-                return response()->api(200,$message,$data);
+            $resp = \Response::make();
+            $content = new JsonResponseContent($_code = $code, $_message = $message, $_data = $data );
+            $resp->setContent($content);
+            $resp->setStatusCode(200,$message);
+            return $resp;  
         });
         
         \Response::macro('deleted',function($message = 'Ressource Deleted', $data = null) {
-            return response()->api(200,$message,$data);          
+            $resp = \Response::make();
+            $content = new JsonResponseContent($_code = $code, $_message = $message, $_data = $data );
+            $resp->setContent($content);
+            $resp->setStatusCode(200,$message);
+            return $resp;            
         });
 
         \Response::macro('updated',function($message = 'Ressource Updated', $data = null) {
-            return response()->api(200,$message,$data);           
+            $resp = \Response::make();
+            $content = new JsonResponseContent($_code = $code, $_message = $message, $_data = $data );
+            $resp->setContent($content);
+            $resp->setStatusCode(200,$message);
+            return $resp;             
         });
 
          \Response::macro('bad_request_exception',function($message = 'You may have some data missing', $data = null) {
-            return response()->api(400,$message,$data);           
+            $resp = \Response::make();
+            $content = new JsonResponseContent($_code = $code, $_message = $message, $_data = $data );
+            $resp->setContent($content);
+            $resp->setStatusCode(400,$message);
+            return $resp;             
         });
         
         \Response::macro('unautherized_exception',function($message = 'Unautherized.', $data = null) {
-            return response()->api(401,$message,$data);           
+            $resp = \Response::make();
+            $content = new JsonResponseContent($_code = $code, $_message = $message, $_data = $data );
+            $resp->setContent($content);
+            $resp->setStatusCode(401,$message);
+            return $resp;            
         });
         
         \Response::macro('internal_server_error',function($message = 'Oops, An Error Occured', $data = null) {
-            return response()->api(500,$message,$data);           
+            $resp = \Response::make();
+            $content = new JsonResponseContent($_code = $code, $_message = $message, $_data = $data );
+            $resp->setContent($content);
+            $resp->setStatusCode(500,$message);
+            return $resp;            
         });
 
         \Response::macro('not_found_exception',function($message = 'Page Not Found', $data = null) {
-            return response()->api(404,$message,$data);           
+            $resp = \Response::make();
+            $content = new JsonResponseContent($_code = $code, $_message = $message, $_data = $data );
+            $resp->setContent($content);
+            $resp->setStatusCode(404,$message);
+            return $resp;            
         });   
     }
 
